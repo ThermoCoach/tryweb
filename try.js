16d6680f10bf29cd493b4252fea018ddfeef010c
ThermoCoach <dnp6ga@virginia.edu>
2013-09-23 11:32:28 -0400

    js

---
+++ try.js
@@ -0,0 +1,26 @@
+
+
+function a() {
+$.ajax({
+            type: "POST",
+            url: "http://smarthome.cs.virginia.edu/webservice/try2.psp",
+            data: ({
+                username: "kamin",
+                password: "kamin"
+            }),
+            cache: false,
+            async: false,
+            dataType: "json",
+            success: eventsbind,
+            error: function(xhr, status, error) {
+  //var err = eval("(" + xhr.responseText + ")");
+  alert(xhr.responseText);
+}
+        });
+
+        function eventsbind(data) {
+            alert(data);
+            events = data;
+
+        }
+}
